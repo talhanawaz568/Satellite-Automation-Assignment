27073b0c21d1fed499538699e99ff55e91542ebb
talhanawaz568 <alnafi789@gmail.com>
2026-06-11 08:08:23 +0500

    new commit

--- pythonProject1/post_quantum/pqc_validator.py
+++ pythonProject1/post_quantum/pqc_validator.py
@@ -2,8 +2,28 @@ import hashlib
 
 def pqc_validate(firmware_data):
 
-    pqc_hash = hashlib.sha512(
-        firmware_data
-    ).hexdigest()
+pqc_hash = hashlib.sha512(
+    firmware_data
+).hexdigest()
 
-    return pqc_hash
+return pqc_hash
+
+if name == "main":
+
+firmware = b"Satellite Firmware"
+
+result = pqc_validate(
+    firmware
+)
+
+print(
+    "\nPOST QUANTUM VALIDATION"
+)
+
+print(
+    "SHA512 Hash:"
+)
+
+print(
+    result
+)
